2d978dcf4c46aa099cefaf8a242787135a7c9bc4
Yeisonlop10 <yeison.lopez1028@gmail.com>
2021-08-14 21:45:28 -0400

    header compponent

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -1,9 +1,14 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 import './header.styles.scss';
 
-const Header = () => {
-
-}
+const Header = () => (
+    <div className='header'>
+        <Link to="/">
+            
+        </Link>
+    </div>
+)
 
 export default Header;
